1b47ec7850a24f932225c30410a699e7c00491ca
Татьяна Жигадло <t.zhyhadlo@gmail.com>
2020-04-29 22:30:06 +0300

    Added migration

--- DB/Context.cs
+++ DB/Context.cs
@@ -8,6 +8,9 @@ namespace LicenseProject.Models
         public DbSet<Module>Moduls { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<DeveloperTeam> DeveloperTeams { get; set; }
+        public DbSet<Selling> Sellings { get; set; }
+        public DbSet<LicenseType> LicenseTypes { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
         public Context(DbContextOptions<Context> options): base(options)
         {
 
---
+++ Migrations/20200429192235_AllModels.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LicenseProject.Migrations
+{
+    public partial class AllModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeveloperTeams",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    PeopleQuantity = table.Column<string>(nullable: true),
+                    SoftName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeveloperTeams", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DiscountName = table.Column<string>(nullable: true),
+                    Percantage = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LicenseTypes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LicenseTypes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moduls",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    SoftName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moduls", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellings",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    InvoiceNumber = table.Column<int>(nullable: false),
+                    CustomerName = table.Column<string>(nullable: true),
+                    SoftName = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    LicenseType = table.Column<string>(nullable: true),
+                    DiscountName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellings", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Softs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Direction = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Softs", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "DeveloperTeams");
+
+            migrationBuilder.DropTable(
+                name: "Discounts");
+
+            migrationBuilder.DropTable(
+                name: "LicenseTypes");
+
+            migrationBuilder.DropTable(
+                name: "Moduls");
+
+            migrationBuilder.DropTable(
+                name: "Sellings");
+
+            migrationBuilder.DropTable(
+                name: "Softs");
+        }
+    }
+}
